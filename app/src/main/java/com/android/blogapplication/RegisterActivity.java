1fba82995132ae2aa8a1e062bbabeb04d66c46af
Riya Panchal <riyap1231@gmail.com>
2021-04-07 21:25:16 +0530

    login_register

---
+++ app/src/main/java/com/android/blogapplication/RegisterActivity.java
@@ -0,0 +1,35 @@
+package com.android.blogapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    private TextView info_text;
+    private Button login_btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+        getwidgets();
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i =new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+    private void getwidgets() {
+        login_btn=findViewById(R.id.login_btn);
+        info_text=findViewById(R.id.info_text);
+    }
+}
