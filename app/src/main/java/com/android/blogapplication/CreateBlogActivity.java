ca662c23a7deb82b18f3e271107e19bd538b5ca5
meezan-mallick <meezan.mallick@gmail.com>
2021-05-15 16:50:25 +0530

    Rich text editor added on createBlogActivity

--- app/src/main/java/com/android/blogapplication/CreateBlogActivity.java
+++ app/src/main/java/com/android/blogapplication/CreateBlogActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.chinalwb.are.AREditor;
+
 public class CreateBlogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_blog);
+
+        AREditor arEditor = this.findViewById(R.id.areditor);
+        arEditor.setExpandMode(AREditor.ExpandMode.FULL);
+        arEditor.setHideToolbar(false);
+        arEditor.setToolbarAlignment(AREditor.ToolbarAlignment.BOTTOM);
+
     }
 }
